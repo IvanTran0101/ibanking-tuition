1e8a8373f1a557e3c7edafbf7dada3cde9888cdc
IvanTran0101 <ivanworkspace0101@gmail.com>
2025-10-29 13:18:04 +0700

    update_publisher

--- libs/rmq/publisher.py
+++ libs/rmq/publisher.py
@@ -4,19 +4,16 @@ import time
 from typing import Dict, Any, Optional
 from .bus import publish
 
-def publish_event(routing_key: str,
-                  payload: Dict[str, Any],
-                  *,
-                  event_type: str,
-                  event_version: str = "v1",
-                  idempotency_key: Optional[str] = None,
-                  correlation_id: Optional[str] = None) -> None:
-    """
-    Chuẩn hoá header cho mọi event.
-    """
+def publish_event(
+    routing_key: str,
+    payload: Dict[str, Any],
+    *,
+    event_type: str,
+    idempotency_key: Optional[str] = None,
+    correlation_id: Optional[str] = None
+) -> None:
     headers = {
         "event-type": event_type,
-        "event-version": event_version,
         "occurred-at": int(time.time() * 1000),
     }
     if idempotency_key:
@@ -25,3 +22,4 @@ def publish_event(routing_key: str,
         headers["correlation-id"] = correlation_id
 
     publish(routing_key=routing_key, body=payload, headers=headers)
+
